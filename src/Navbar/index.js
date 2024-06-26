358f73d08c01c6c97daecd9d0fe4a20c416966b0
Kur-Achan <achananyiethkur@gmail.com>
2024-06-26 19:54:14 +0300

    add files

---
+++ src/Navbar/index.js
@@ -0,0 +1,20 @@
+import  './index.css';
+const Navbar = ()=>{
+
+    return(
+    
+<div className='nav'>
+    <p>+ Contact Us</p>
+    <h1>Perfume Forever</h1>
+    <p>Flash Sales 15% off on all women's Perfume</p>
+    
+</div>
+    );
+};
+
+
+export default Navbar;
+
+
+
+
